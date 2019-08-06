4405317e249eb654a1eb6a126817d5f824fa5529
Kalana Vimukthi Weerarathna <kalana.weerarathne@scicom.com.my>
2019-08-06 10:55:18 +0530

    -docker setup

--- cart-service/src/main/java/com/example/cartservice/controller/CartController.java
+++ cart-service/src/main/java/com/example/cartservice/controller/CartController.java
@@ -1,31 +1,14 @@
 package com.example.cartservice.controller;
 
-import com.example.cartservice.dao.Item;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * Copyright (c) 2019. scicom.com.my - All Rights Reserved
  * Created by kalana.w on 6/20/2019.
  */
 @RestController
-@RequestMapping(name = "cart")
+@RequestMapping("cart")
 public class CartController
 {
-	@GetMapping("")
-	ResponseEntity<List<Item>> findAllCartDetails()
-	{
-		List<Item> items = new ArrayList<>();
-		items.add( new Item( 1, "Pen" , 12.0, 150 ) );
-		items.add( new Item( 1, "Pencil" , 5.0, 80 ) );
-		items.add( new Item( 1, "Eraser" , 10.0, 200 ) );
-		items.add( new Item( 1, "A4 Bundle" , 500.0, 500 ) );
-
-		return ResponseEntity.ok( items );
-	}
 }
--- cart-service/src/test/java/com/example/cartservice/CartServiceApplicationTests.java
+++ cart-service/src/test/java/com/example/cartservice/CartServiceApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.orderservice;
+package com.example.cartservice;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,10 +7,12 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class OrderServiceApplicationTests {
+public class CartServiceApplicationTests
+{
 
-    @Test
-    public void contextLoads() {
-    }
+	@Test
+	public void contextLoads()
+	{
+	}
 
 }
